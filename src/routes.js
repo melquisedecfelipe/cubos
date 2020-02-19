7bdfb9a4981dddc41a58bbf885c075bb1a106789
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-02-18 22:26:27 -0300

    feat: update routes

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Detail from './pages/Detail';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/detail/:id" component={Detail} />
     </Switch>
   );
 }
