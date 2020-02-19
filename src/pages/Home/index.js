b68de0a93644aa0daef51f4eeba3b472ffe8c779
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-02-19 14:21:33 -0300

    feat: update home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,46 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import './styles.scss';
 
-import Button from '../../components/Button';
-import Films from '../../components/Film';
+import Films from '../../components/Films';
 import Input from '../../components/Input';
 import Paginate from '../../components/Paginate';
 import Select from '../../components/Select';
 
 import api from '../../services/api';
+import { Creators as FilmActions } from '../../store/ducks/film';
 
-export default function Home() {
+function Home({ films, setFilms, getAll }) {
   const [name, setName] = useState('');
   const [genre, setGenre] = useState('');
   const [genres, setGenres] = useState('');
 
   const [currentPage, setCurrentPage] = useState(1);
-  const [films, setFilms] = useState(undefined);
   const [totalPages, setTotalPages] = useState(undefined);
 
   const paginate = pageNumber => setCurrentPage(pageNumber);
 
   const queryString = `api_key=563ff87b1a264a4d94dfacd2b4fc22fd&language=pt-BR`;
-
-  async function handleSearch(e) {
-    if (e !== undefined) {
-      e.preventDefault();
-    }
-
-    try {
-      const { data } = await api.get(`movie/now_playing?${queryString}&page=${currentPage}`);
-      setFilms(data.results);
-      setTotalPages(5);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    handleSearch();
-  }, [currentPage]);
 
   useEffect(() => {
     async function getGenres() {
@@ -50,15 +33,40 @@ export default function Home() {
     }
 
     getGenres();
-  }, [queryString]);
+  }, [currentPage, queryString]);
+
+  useEffect(() => {
+    async function getFilms() {
+      if (genre !== '') {
+        const { data } = await api.get(
+          `discover/movie?${queryString}&page=${currentPage}&with_genres=${genre}`,
+        );
+
+        setFilms(data.results);
+        setTotalPages(5);
+      } else {
+        const { data } = await api.get(`discover/movie?${queryString}&page=${currentPage}`);
+
+        setFilms(data.results);
+        setTotalPages(5);
+      }
+
+      const storageFilms = JSON.parse(localStorage.getItem('films'));
+      getAll(storageFilms);
+    }
+
+    getFilms();
+  }, [getAll, setFilms, genre, currentPage, queryString]);
 
   function handleName(e) {
     setName(e.target.value);
   }
 
-  function handleGenre(e) {
+  async function handleGenre(e) {
     setGenre(e);
   }
+
+  const filteredFilms = films.filter(film => film.title.toLowerCase().includes(name.toLowerCase()));
 
   return (
     <div className="home-container">
@@ -66,34 +74,43 @@ export default function Home() {
         <header>
           <Link to="/">FILM</Link>
         </header>
-        <form>
+        <div>
           <div className="header">
             <h2>Qual filme você quer ver hoje?</h2>
             <p>Selecione os filtros e seja feliz.</p>
           </div>
           <div className="search">
-            <Input placeholder="Nome" defaultValue={name} onBlur={handleName} />
-            <div>
-              <Select placeholder="Genero" value={genre} onChange={handleGenre} option={genres} />
-              <Button classe="-white" label="Buscar" onClick={handleSearch} />
-            </div>
+            <Input defaultValue={name} onBlur={handleName} />
+            <Select value={genre} onChange={handleGenre} option={genres} />
           </div>
-        </form>
+        </div>
       </div>
 
-      {films !== undefined && (
-        <div className="home-content">
-          <div className="film">
-            <Films films={films} />
-          </div>
+      <div className="home-content">
+        <div className="film">
+          {filteredFilms.length > 0 ? (
+            <Films films={filteredFilms} />
+          ) : (
+            <h1>Não encontramos nenhum item com esse filtro... :(</h1>
+          )}
+        </div>
 
+        {!(name !== '') && (
           <div className="footer">
             <div className="paginate">
               <Paginate currentPage={currentPage} totalPages={totalPages} paginate={paginate} />
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
+
+const mapStateToProps = state => ({
+  films: state.film.films,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(FilmActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
