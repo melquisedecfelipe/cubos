757878a862f97cd10ba71b42162552ec114b76be
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-02-18 22:26:50 -0300

    feat: update component home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles.scss';
 
@@ -10,21 +11,18 @@ import Select from '../../components/Select';
 
 import api from '../../services/api';
 
-import optionsSelect from './optionsSelect';
-import Film from '../../components/Film';
-
-export default function Search() {
+export default function Home() {
   const [name, setName] = useState('');
   const [genre, setGenre] = useState('');
   const [genres, setGenres] = useState('');
 
   const [currentPage, setCurrentPage] = useState(1);
-  const [filmPerPage, setFilmPerPage] = useState(12);
   const [films, setFilms] = useState(undefined);
   const [totalPages, setTotalPages] = useState(undefined);
 
-  const queryString = `api_key=563ff87b1a264a4d94dfacd2b4fc22fd&language=pt-BR`;
   const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  const queryString = `api_key=563ff87b1a264a4d94dfacd2b4fc22fd&language=pt-BR`;
 
   async function handleSearch(e) {
     if (e !== undefined) {
@@ -32,7 +30,7 @@ export default function Search() {
     }
 
     try {
-      const { data } = await api.get(`movie/now_playing?${queryString}`);
+      const { data } = await api.get(`movie/now_playing?${queryString}&page=${currentPage}`);
       setFilms(data.results);
       setTotalPages(5);
     } catch (error) {
@@ -42,7 +40,7 @@ export default function Search() {
 
   useEffect(() => {
     handleSearch();
-  }, [currentPage, filmPerPage]);
+  }, [currentPage]);
 
   useEffect(() => {
     async function getGenres() {
@@ -52,69 +50,50 @@ export default function Search() {
     }
 
     getGenres();
-  }, []);
-
-  function handleFilmPerPage(e) {
-    setFilmPerPage(e);
-  }
+  }, [queryString]);
 
   function handleName(e) {
     setName(e.target.value);
   }
 
   function handleGenre(e) {
-    setGenre(e.target.value);
+    setGenre(e);
   }
 
   return (
-    <>
-      <div
-        className={
-          films !== undefined && films.length !== 0 ? 'search-container -films' : 'search-container'
-        }
-      >
+    <div className="home-container">
+      <div className="search-container">
         <header>
-          <h1>FILM</h1>
+          <Link to="/">FILM</Link>
         </header>
         <form>
           <div className="header">
-            <h1>Buscar</h1>
-            <div>
-              <h2>Qual filme você quer ver hoje?</h2>
-              <p>selecione os filtros e seja feliz.</p>
-            </div>
+            <h2>Qual filme você quer ver hoje?</h2>
+            <p>Selecione os filtros e seja feliz.</p>
           </div>
           <div className="search">
             <Input placeholder="Nome" defaultValue={name} onBlur={handleName} />
-            <Select placeholder="Genero" value={genre} onChange={handleGenre} option={genres} />
-            <Button classe="-white" label="Buscar" onClick={handleSearch} />
+            <div>
+              <Select placeholder="Genero" value={genre} onChange={handleGenre} option={genres} />
+              <Button classe="-white" label="Buscar" onClick={handleSearch} />
+            </div>
           </div>
         </form>
       </div>
+
       {films !== undefined && (
-        <>
-          <Films films={films} />
+        <div className="film-container">
+          <div className="film">
+            <Films films={films} />
+          </div>
+
           <div className="footer">
-            <div className="register">
-              <div className="select">
-                <p>Mostrando</p>
-                <Select value={filmPerPage} onChange={handleFilmPerPage} option={optionsSelect} />
-                <p>
-                  de <strong>{films.count}</strong> registros encontrados.{' '}
-                </p>
-              </div>
-              <div className="paginate">
-                <Paginate
-                  currentPage={currentPage}
-                  filmPerPage={filmPerPage}
-                  totalPages={totalPages}
-                  paginate={paginate}
-                />
-              </div>
+            <div className="paginate">
+              <Paginate currentPage={currentPage} totalPages={totalPages} paginate={paginate} />
             </div>
           </div>
-        </>
+        </div>
       )}
-    </>
+    </div>
   );
 }
